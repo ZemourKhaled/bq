66b9b46cfe9c9c644ca59e56d26c5825fb8bd38b
DaMi <khaledzemour@gmail.com>
2021-05-30 15:19:40 +0200

    first commit

---
+++ src/main/java/com/example/bq/BqApplication.java
@@ -0,0 +1,13 @@
+package com.example.bq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bq/BqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
